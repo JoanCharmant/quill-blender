c5cf35571731189a9802be0fd22d1d8271033e3f
Joan Charmant <jcharmant@gmail.com>
2024-12-26 15:14:02 +0000

    Export: Grease Pencil: Generate vertices for round and flat caps. Support single-point round cap.

--- io_scene_quill/exporters/paint_gpencil.py
+++ io_scene_quill/exporters/paint_gpencil.py
@@ -1,5 +1,6 @@
 import bpy
 import logging
+import math
 import mathutils
 from ..model import sequence, paint
 from . import utils
@@ -95,10 +96,17 @@ def make_paint_layer(gpencil_layer, gpencil_materials, config):
         # Convert all Grease Pencil strokes to Quill ones.
         for gpencil_stroke in gpencil_frame.strokes:
 
+            # Ignore single-point GPencil strokes using flat cap as they can't really be represented in Quill.
+            # For round cap this will create a perfect sphere.
+            if len(gpencil_stroke.points) < 2 and gpencil_stroke.start_cap_mode == 'FLAT':
+                continue
+
             # Surface component (Stroke, Fill or both).
-            # If neither are enabled or the drawing has no strokes, it is still
-            # a valid drawing, just empty.
             material = gpencil_materials[gpencil_stroke.material_index].grease_pencil
+
+            # Bypass if neither are enabled.
+            if not material.show_stroke and not material.show_fill:
+                continue
 
             if material.show_stroke:
                 # This can be represented by a normal Quill stroke.
@@ -176,6 +184,7 @@ def make_normal_stroke(gpencil_stroke, material, thickness_offset):
     bbox = utils.bbox_empty()
     vertices = []
     for i in range(len(gpencil_stroke.points)):
+
         gpencil_point = gpencil_stroke.points[i]
         p = utils.swizzle_yup_location(gpencil_point.co)
 
@@ -196,6 +205,13 @@ def make_normal_stroke(gpencil_stroke, material, thickness_offset):
         vertex = paint.Vertex(p, normal, tangent, color, opacity, width)
         vertices.append(vertex)
         bbox = utils.bbox_add_point(bbox, p)
+
+    # Add extra vertices for caps.
+    # We only do this if the first point has a width. This is used to detect if the GPencil
+    # stroke is already imported from Quill and we don't need to add caps.
+    # This means we don't quite support round-trip of Quill strokes with no caps.
+    if vertices[0].width > 0:
+        add_caps(vertices, gpencil_stroke.start_cap_mode, bbox)
 
     id = 0
     return paint.Stroke(id, bbox, brush_type, disable_rotational_opacity, vertices)
@@ -231,8 +247,102 @@ def compute_tangent(gpencil_stroke, i, p):
     # If that's still zero, go for a desperate solution - overal stroke direction + noise.
     last = utils.swizzle_yup_location(gpencil_stroke.points[-1].co)
     first = utils.swizzle_yup_location(gpencil_stroke.points[0].co)
-    yaxis = (last - first + mathutils.Vector(0.000001, 0.000002, 0.000003)).normalized()
+    yaxis = (last - first + mathutils.Vector((0.000001, 0.000002, 0.000003))).normalized()
     return yaxis
+
+def add_caps(vertices, caps_type, bbox):
+
+    # Create the caps if needed.
+    # We assume both start and end caps are the same, it's seemingly impossible to
+    # set different start and end cap types from Blender UI.
+
+    # If the first point already has zero width we assume this is already
+    # imported from Quill and we don't need to add caps.
+    if vertices[0].width == 0:
+        return
+
+    round_cap_segments = 5
+
+    if caps_type == 'FLAT':
+
+        # Note: we extend the stroke by a small length on each side.
+        # If we set the extra points exactly at the same location as the current end points
+        # it behaves badly when imported back.
+
+        # Start cap
+        p = vertices[0].position - (vertices[0].tangent * vertices[0].width / 10)
+        normal = vertices[0].normal
+        tangent = vertices[0].tangent
+        color = vertices[0].color
+        opacity = vertices[0].opacity
+        width = 0
+        vertex = paint.Vertex(p, normal, tangent, color, opacity, width)
+        vertices.insert(0, vertex)
+
+        # End cap
+        p = vertices[-1].position + (vertices[-1].tangent * vertices[-1].width / 10)
+        normal = vertices[-1].normal
+        tangent = vertices[-1].tangent
+        color = vertices[-1].color
+        opacity = vertices[-1].opacity
+        width = 0
+        vertex = paint.Vertex(p, normal, tangent, color, opacity, width)
+        vertices.append(vertex)
+
+    elif caps_type == 'ROUND':
+
+        def make_hemisphere(vertices, start_index, end_index, radius, count):
+            # Create intermediate points to make a hemisphere.
+            # start_index is at the base of the hemisphere.
+            extra_vertices = []
+            for i in range(count):
+                k = (i+1)/(count+1)
+                dir = (vertices[end_index].position - vertices[start_index].position).normalized()
+                p = vertices[start_index].position + dir * (k * radius)
+                dist = (p - vertices[start_index].position).length
+                angle = math.acos(dist / radius)
+                width = math.sin(angle) * radius
+
+                normal = vertices[start_index].normal
+                tangent = vertices[start_index].tangent
+                color = vertices[start_index].color
+                opacity = vertices[start_index].opacity
+
+                vertex = paint.Vertex(p, normal, tangent, color, opacity, width)
+                extra_vertices.append(vertex)
+
+            return extra_vertices
+
+        # Start cap
+        p = vertices[0].position - (vertices[0].tangent * vertices[0].width)
+        normal = vertices[0].normal
+        tangent = vertices[0].tangent
+        color = vertices[0].color
+        opacity = vertices[0].opacity
+        width = 0
+        vertex = paint.Vertex(p, normal, tangent, color, opacity, width)
+        vertices.insert(0, vertex)
+        bbox = utils.bbox_add_point(bbox, p)
+        extra_vertices = make_hemisphere(vertices, 1, 0, vertices[1].width, round_cap_segments)
+        for i in range(len(extra_vertices)):
+            bbox = utils.bbox_add_point(bbox, extra_vertices[i].position)
+            vertices.insert(1, extra_vertices[i])
+
+        # End cap
+        p = vertices[-1].position + (vertices[-1].tangent * vertices[-1].width)
+        normal = vertices[-1].normal
+        tangent = vertices[-1].tangent
+        color = vertices[-1].color
+        opacity = vertices[-1].opacity
+        width = 0
+        vertex = paint.Vertex(p, normal, tangent, color, opacity, width)
+        vertices.append(vertex)
+        bbox = utils.bbox_add_point(bbox, p)
+        extra_vertices = make_hemisphere(vertices, len(vertices) - 2, len(vertices) - 1, vertices[-2].width, round_cap_segments)
+        for i in range(len(extra_vertices)):
+            bbox = utils.bbox_add_point(bbox, extra_vertices[i].position)
+            vertices.insert(len(vertices) - 1, extra_vertices[i])
+
 
 def make_fill_stroke(gpencil_stroke, material):
 
